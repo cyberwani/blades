f8364bbccf92178177d0d1469081c22a3a3fa2ff
Jared Novack <jarednova@upstatement.com>
2013-07-25 21:35:35 -0400

    page-specific CSS files

--- page.php
+++ page.php
@@ -12,19 +12,9 @@
  * @since Boilerplate 1.0
  */
 
-get_header(); ?>
-<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
-	<?php
-		$data = array();
-		$pi = PostMaster::get_post_info(get_the_ID());
+
+		$data = Timber::get_context();
+		$pi = new TimberPost();
 		$data['post'] = $pi;
-		$template_file = 'page.html';
-		if (file_exists(__DIR__.'/views/page-'.$pi->post_name.'.html')){
-			$template_file = 'page-'.$pi->post_name.'.html';
-		} 
-		render_twig($template_file, $data);
-	?>
-<?php endwhile; // end of the loop. ?>
-
-
-<?php get_footer(); ?>
+		$template_file = 'page.twig';
+		Timber::render(['page-'.$pi->post_name.'.twig', 'page.twig'], $data);
