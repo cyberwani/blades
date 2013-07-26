2959d1f0768a9a4b30f85f7d3d0a88c8486029fb
Jared Novack <jarednova@upstatement.com>
2013-07-25 20:37:21 -0400

    more adjustments and movements

--- page-jobs.php
+++ page-jobs.php
@@ -1,33 +1,13 @@
 <?php
-/**
- * The template for displaying all pages.
- *
- * This is the template that displays all pages by default.
- * Please note that this is the wordpress construct of pages
- * and that other 'pages' on your wordpress site will use a
- * different template.
- *
- * @package WordPress
- * @subpackage Boilerplate
- * @since Boilerplate 1.0
- */
-
-get_header(); ?>
-<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
-	<?php
-		$data = array();
-		$pi = PostMaster::get_post_info(get_the_ID());
-		$data['post'] = $pi;
-		$jobs = get_field('jobs');
-		$data['jobs'] = $jobs;
-
-		$template_file = 'page.html';
-		if (file_exists(__DIR__.'/views/page-'.$pi->post_name.'.html')){
-			$template_file = 'page-'.$pi->post_name.'.html';
-		} 
-		render_twig($template_file, $data);
-	?>
-<?php endwhile; // end of the loop. ?>
 
 
-<?php get_footer(); ?>
+	$data = Timber::get_context();
+	$pi = new TimberPost();
+	$data['post'] = $pi;
+	$data['jobs'] = get_field('jobs', $pi->ID);
+
+	$template_file = 'page.twig';
+	if (file_exists(__DIR__.'/views/page-'.$pi->post_name.'.twig')){
+		$template_file = 'page-'.$pi->post_name.'.twig';
+	} 
+	Timber::render($template_file, $data);
