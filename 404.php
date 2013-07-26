074ac055b99588f169d4bc18f779f58b3db294fa
Jared Novack <jarednova@upstatement.com>
2013-07-25 21:24:33 -0400

    trashing old .html files

--- 404.php
+++ 404.php
@@ -1,17 +1,6 @@
 <?php
-/**
- * The template for displaying 404 pages (Not Found).
- *
- * @package WordPress
- * @subpackage Boilerplate
- * @since Boilerplate 1.0
- */
+	$data = Timber::get_context();
+	//$data['post'] = PostMaster::get_post_info(2611);
+	Timber::render('404.twig', $data);
 
-get_header(); ?>
-			<article id="post-0" class="post error404 not-found" role="main">
-				<?php
-					$data['post'] = PostMaster::get_post_info(2611);
-					render_twig('404.html', $data);
-				?>
-			</article>
-<?php get_footer(); ?>
+
