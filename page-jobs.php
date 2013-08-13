1828ebe3347b9ec622d7986e346cc21323829ce7
Jared Novack <jarednova@upstatement.com>
2013-08-13 10:01:59 -0400

    Updating jobs page

--- page-jobs.php
+++ page-jobs.php
@@ -6,8 +6,4 @@
 	$data['post'] = $pi;
 	$data['jobs'] = get_field('jobs', $pi->ID);
 
-	$template_file = 'page.twig';
-	if (file_exists(__DIR__.'/views/page-'.$pi->post_name.'.twig')){
-		$template_file = 'page-'.$pi->post_name.'.twig';
-	} 
-	Timber::render($template_file, $data);
+	Timber::render(['page-'.$pi->post_name.'.twig', 'page.twig'], $data);
