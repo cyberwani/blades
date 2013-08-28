494a4617739ec7f1152ed00f3874edf79c58be2b
Jared Novack <jared@upstatement.com>
2013-08-27 22:23:31 -0400

    Update acf-blog-options.php

--- wp/acf-blog-options.php
+++ wp/acf-blog-options.php
@@ -6,22 +6,6 @@
  * Since all activation codes are multi-site licenses, you are allowed to include your key in premium themes.
  */ 
 
-function my_acf_settings( $options )
-{
-    // activate add-ons
-    $options['activation_codes']['repeater'] = 'QJF7-L4IX-UCNP-RF2W';
-    $options['activation_codes']['options_page'] = 'OPN8-FA4J-Y2LW-81LS';
-    $options['activation_codes']['flexible_content'] = 'FC9O-H6VN-E4CL-LT33';
-    $options['activation_codes']['gallery'] = 'XXXX-XXXX-XXXX-XXXX';
-    
-    // setup other options (http://www.advancedcustomfields.com/docs/filters/acf_settings/)
-    
-    return $options;
-    
-}
-add_filter('acf_settings', 'my_acf_settings');
-
-
 /**
  * Register field groups
  * The register_field_group function accepts 1 array which holds the relevant data to register a field group
