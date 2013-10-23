78fbfe3f74925871b20e962deceb4f563eb3598c
Jared Novack <jarednova@upstatement.com>
2013-10-23 10:24:33 -0400

    adding preceding slash to theme_dir

--- functions.php
+++ functions.php
@@ -69,29 +69,7 @@
 			Timber::render('admin/portfolio-square-preview.twig', $data);
 		}, -1000);
 	}
-	/*
-	Timber::add_route('blog', function($params) use ($routes){
-		$sticky = get_option('sticky_posts');
-		$sticky = WPHelper::array_truncate($sticky, 4);
-		$page = 0;
-		$query = array('post_type' => 'post', 'posts_per_page' => 6, 'post__not_in' => $sticky, 'offset' => $page * 6);
-		$routes->load('archive-blog.php', $query);
-	});
 
-
-	Timber::add_route('blog/page/:pg', function($params) use ($routes){
-		$sticky = get_option('sticky_posts');
-		$sticky = WPHelper::array_truncate($sticky, 4);
-		$page = $params['pg'];
-		$page -= 1;
-		$page = max(0, $page);
-		$query = array('post_type' => 'post', 'posts_per_page' => 6, 'post__not_in' => $sticky, 'offset' => $page * 6);
-		$routes->load('archive-blog.php', $query);
-	});*/
-
-
-
-	
 	Timber::add_route('blog', function($params){
 		$sticky = get_option('sticky_posts');
 		$sticky = WPHelper::array_truncate($sticky, 4);
@@ -109,5 +87,3 @@
 		$query = array('post_type' => 'post', 'posts_per_page' => 6, 'post__not_in' => $sticky, 'offset' => $page * 6);
 		Timber::load_template('archive-blog.php', $query);
 	});
-	
-
