a53c238a2452097e9df8f3c461454dea4e9cc93c
Tito Bottitta <tito@upstatement.com>
2014-03-31 10:59:22 -0400

    * Added features slider section to B.com portfolio post. * Created an accessible "Scripts" block in base.twig * Removed inaccessible "Scripts" block from footer.twig

--- js/portfolio/bcom.js
+++ js/portfolio/bcom.js
@@ -8,68 +8,117 @@
  */
 (function($) {
 
+	// Initialize Swiper
+	function initBcomSwiper() {
+
+		var speedOrigin = 10,
+			speedButton = speedOrigin + 100;
+
+		var bcomSwiper = $('.swiper-img-mod').swiper({
+			mode:'horizontal',
+			loop: true,
+			speed: speedOrigin,
+			calculateHeight: true,
+            onSlideChangeEnd: function() {
+                $('.bcom-feat-mod').removeClass('transition-active');
+                var slide = bcomSwiper.activeSlide();
+            },
+		    onSlideNext:function(swiper){
+				bcomSwiper_caption.swipeNext();
+		    },
+		    onSlidePrev:function(swiper){
+				bcomSwiper_caption.swipePrev();
+		    }
+		});
+
+		var bcomSwiper_caption = $('.swiper-caption-mod').swiper({
+			mode:'horizontal',
+			loop: true,
+			speed: speedOrigin,
+			calculateHeight: true
+		});
+
+		$('.bcom-feat-prev').on('click', function() {
+            $('.bcom-feat-mod').addClass('transition-active');
+            setTimeout(function(){
+				bcomSwiper.swipePrev();
+            }, speedButton);
+		});
+		$('.bcom-feat-next').on('click', function() {
+            $('.bcom-feat-mod').addClass('transition-active');
+            setTimeout(function(){
+				bcomSwiper.swipeNext();
+            }, speedButton);		
+        });
+
+	} // bcom_swiper
+
+	initBcomSwiper();
+
+
+
 	// window.setInterval(triggerNextFast, 7000);
-if (!$.support.transition)
-    $.fn.transition = $.fn.animate;
-	// triggerNextFast();
+	if (!$.support.transition)
+	    $.fn.transition = $.fn.animate;
+		// triggerNextFast();
 
-	'use strict';
+		'use strict';
 
-	// browser button stuff
-	var wHeight = $(window).height();
-
-	$(window).on('resize', function(){
-		if(!$('html').hasClass('touch')){
-			resizeThings();
-		}
-		heightsOnSize();
-	});
-
-	function resizeThings() {
+		// browser button stuff
 		var wHeight = $(window).height();
-		$('.bcom-stream-section').css('max-height', (wHeight) + 'px');
+
+		$(window).on('resize', function(){
+			if(!$('html').hasClass('touch')){
+				resizeThings();
+			}
+			heightsOnSize();
+		});
+
+		function resizeThings() {
+			var wHeight = $(window).height();
+			$('.bcom-stream-section').css('max-height', (wHeight) + 'px');
+		}
+
+
+		var filterh;
+		var streamButtonh;
+		var stream1h;
+		var stream2h;
+		var stream3h;
+		var stream4h;
+		var stream5h;
+		var stream6h;
+		var stream7h;
+
+
+		// Everything in a set timeout so we can get element heights
+
+	function heightsOnSize() {
+		filterh = $('.stream-img-filter').height();
+		streamButtonh = $('.stream-img-button').height();
+		stream3h = $('.stream-img-3').height();
+		stream4h = $('.stream-img-4').height();
+		stream5h = $('.stream-img-5').height();
+		stream6h = $('.stream-img-6').height();
+		stream7h = $('.stream-img-7').height();
+
+		if($(window).width() > 600) {
+			stream1h = (filterh);
+		} else {
+			stream1h = (0);
+		}
 	}
 
-
-	var filterh;
-	var streamButtonh;
-	var stream1h;
-	var stream2h;
-	var stream3h;
-	var stream4h;
-	var stream5h;
-	var stream6h;
-	var stream7h;
-
-
-	// Everything in a set timeout so we can get element heights
-
-function heightsOnSize() {
-	filterh = $('.stream-img-filter').height();
-	streamButtonh = $('.stream-img-button').height();
-	stream3h = $('.stream-img-3').height();
-	stream4h = $('.stream-img-4').height();
-	stream5h = $('.stream-img-5').height();
-	stream6h = $('.stream-img-6').height();
-	stream7h = $('.stream-img-7').height();
-
-	if($(window).width() > 600) {
-		stream1h = (filterh);
-	} else {
-		stream1h = (0);
+	function heightsOnClick() {
+		stream2h = $('.stream-img-2').height();
 	}
-}
 
-function heightsOnClick() {
-	stream2h = $('.stream-img-2').height();
-}
+	function getHeights() {
+		heightsOnClick();
+		heightsOnSize();
+		console.log('new heights');
 
-function getHeights() {
-	heightsOnClick();
-	heightsOnSize();
-	console.log('new heights');
-
-}
+	}
 
 	setTimeout(function() {
 
@@ -240,9 +289,9 @@ function getHeights() {
 		});
 
 		// Inits
-
 		streamInit();
 
 	}, 1000);
 	resizeThings();
+
 })(jQuery);
