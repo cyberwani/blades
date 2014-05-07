1ab5dd3a0fc1cf2857f70d8cbd658556bf16260c
thetmkay <george@upstatement.com>
2014-05-07 18:21:15 -0400

    reworked to be objects - still haven't exported or fixed bug

--- js/custom/portfolio/hlr.js
+++ js/custom/portfolio/hlr.js
@@ -1,6 +1,6 @@
 /*jshint unused:false */
 /*global $:false, _:false, Swiper:false, Hammer:false */
-
+'use strict';
 /*
  * Nav module for Boston.com prototype
  * by Nathan Hass c/o Upstatement (@upstatement)
@@ -8,16 +8,12 @@
  */
 
 (function($) {
-
-	//Things to EDIT
+	var ts,bs;
 
 	//SET SPEED of Background Slide Show
-	var amount_to_move = 1,
-		animation_delay = 30; //Change this one first
-
-	//SET IMAGES of Background Slide Show
-	var NUM_IMAGES = 30,
-		img_paths = [];
+	var AMOUNT_TO_MOVE = 1,
+		ANIMATION_DELAY = 30,
+		NUM_IMAGES = 30;
 
 	//SET IMAGES of Type slider
 	var	resp_imgs = [
@@ -31,188 +27,288 @@
 			}
 		];
 
-	//Background Slide Show
+	function BackgroundSlideshow(img_paths, options) {
 
-	var	$bg_scrubber = $('.hlr-bg-scrubber'),
-		$bg_img_holder = $('.hlr-bg-img'),
-		$bg_timeline = $('.hlr-bg-timeline'),
-		$bg_slider = $('.hlr-bg-slider'),
-		$bg_button = $('.hlr-bg-btn');
+		var _defaults = {
+			scrubber: '.hlr-bg-scrubber',
+			img_holder: '.hlr-bg-img',
+			timeline: '.hlr-bg-timeline',
+			slider: '.hlr-bg-slider',
+			button: '.hlr-bg-btn',
+			play_class: 'hlr-bg-playing',
+			pause_class: 'hlr-bg-paused'
+		};
 
-	var total_width,
-		animation_id;
+		options = $.extend(_defaults,options);
 
+		if(!img_paths) {
+			console.error('Did not specify image paths');
+			return undefined;
+		} else {
+			this._img_paths = img_paths;
+		}
 
-	//helper functions
+		this._scrubber = options.scrubber;
+		this._img_holder = options.img_holder;
+		this._timeline = options.timeline;
+		this._slider = options.slider;
+		this._button = options.button;
+
+		this._play_class = options.play_class;
+		this._pause_class = options.pause_class;
+
+		this._animation_id = false;
+
+		this.initScrubber();
+		this.bindButtonEvent();
+		this.bindInView();
+		this.bindToResize();
+		this.findWidth();
+		//return this;
+	};
+
+	BackgroundSlideshow.prototype = {
+		cacheImages: function() {
+			this._img_paths.forEach(function(img_path) {
+				var img = new Image();
+				img.src = img_path;
+			});
+		},
+		initScrubber: function() {
+			var _this = this;
+			$(_this._scrubber).draggable({
+				drag: function() {
+					_this.updateBackground();
+				},
+				start: function() {
+					_this.pause()
+				},
+				scroll: false,
+				grid: [1,0],
+				containment: _this._timeline
+			});
+
+		},
+		findWidth: function() {
+			this._timeline_width = $(this._timeline).width() - $(this._scrubber).width();
+		},
+		bindButtonEvent: function() {
+			var _this = this;
+			$(this._button).on('click', function(event) {
+				event.preventDefault();
+				if ($(_this._slider).hasClass(_this._pause_class)) {
+					_this.play();
+				}
+				else {
+					_this.pause();
+				}
+			});
+
+		},
+		bindInView: function() {
+			var _this = this;
+			$(this._img_holder).bind('inview', function(event, isInView, visiblePartX, visiblePartY) {
+
+				if(visiblePartY === 'both') {
+					_this.play();
+				}
+				else {
+					_this.pause();
+				}
+
+			});
+
+		},
+		bindToResize: function() {
+			$(window).resize(this.findWidth);
+		},
+		pause: function() {
+			if(this._animation_id) {
+
+				clearInterval(this._animation_id);
+
+				$(this._timeline).unbind('inview');
+
+				this._animation_id = false;
+
+				$(this._slider).removeClass(this._play_class);
+				$(this._slider).addClass(this._pause_class);
+			}
+		},
+		play: function() {
+			if(!this._animation_id)
+			{
+				this._animation_id = setInterval(this.moveScrub, ANIMATION_DELAY, this);
+
+				$(this._slider).removeClass(this._pause_class);
+				$(this._slider).addClass(this._play_class);
+			}
+		},
+		updateBackground: function() {
+			var index = Math.floor(($(this._scrubber).position().left/this._timeline_width)*NUM_IMAGES);
+			$(this._img_holder).attr('src', this._img_paths[index]);
+		},
+		moveScrub: function (_this) {
+			var $bg_scrubber = $(_this._scrubber),
+				left_pos = $bg_scrubber.position().left;
+
+			if(left_pos + AMOUNT_TO_MOVE >= _this._timeline_width) {
+				left_pos = 0;
+			}
+			else {
+				left_pos = left_pos + 1;
+			}
+
+			$bg_scrubber.css({
+				left: left_pos
+			});
+
+			_this.updateBackground();
+		}
+	};
+
+	//Type Slider
+
+	var TypeScrubber = function(options) {
+
+		var _defaults = {
+			type_container: '#hlr-type-container',
+			type_mask: '.hlr-type-mask-mod',
+			bind_touch_events: Modernizr.touch
+		};
+
+		options = $.extend(_defaults,options);
+
+		this._type_container = options.type_container;
+		this._type_mask = options.type_mask;
+
+		this.bindMouseEvents();
+
+		if(options.bind_touch_events) {
+			this.bindTouchEvents();
+		}
+
+		return this;
+	};
+
+	TypeScrubber.prototype = {
+		bindMouseEvents: function() {
+			var $type_container = $(this._type_container),
+				_this = this;
+
+			$type_container.bind('mouseleave', function(event) {
+				_this.resetScrubber(event);
+			});
+			$type_container.bind('mousemove', function(event) {
+				_this.handleMove(event);
+			});
+		},
+		bindTouchEvents: function() {
+			var $type_container = $(this._type_container),
+				_this = this;
+
+			$type_container.on('touchmove touchstart', function(event) {
+					_this.handleMove(event);
+			});
+			$type_container.on('touchend', function(event) {
+				_this.resetScrubber(event);
+			});
+		},
+		handleMove: function(event) {
+			event.preventDefault();
+			var scrub_pos = this.getScrubPosition(event);
+			this.moveScrubber(scrub_pos);
+		},
+		getScrubPosition: function(event) {
+			return this.getOffset(event);
+		},
+		moveScrubber: function(scrub_pos) {
+			$(this._type_mask).css({
+				width: scrub_pos
+			});
+		},
+		resetScrubber: function(event) {
+			event.preventDefault();
+			$(this._type_mask).css({
+				'width': '50%'
+			});
+		},
+		getOffset: function(event) {
+			if(event.type === 'mousemove') {
+				return event.offsetX;
+			} else if (event.type === 'touchmove' || event.type === 'touchstart') {
+				return event.originalEvent.touches[0].pageX - $(this._container).offset().left;
+			} else {
+				console.error('Not Valid Event');
+				console.log(event);
+				return 0;
+			}
+		}
+	};
+
 
 	//Change if hardcode img paths
-	var setUpImgPaths = function() {
-		var img_dir = $bg_img_holder.data('bg-path');
+	var _setUpImgPaths = function() {
+		var img_paths = [],
+			img_dir = $('.hlr-bg-img').data('bg-path');
+
 		for(var i = 1; i <= NUM_IMAGES; i++) {
 			img_paths.push(img_dir + 'bg-' + i + '.jpg');
 		}
+
+		return img_paths;
 	};
 
-	var preloadImages = function(img_arr) {
-		img_arr.forEach(function(img_path) {
-			var img = new Image();
-			img.src = img_path;
+	//Window Bindings
+
+	var _findResponsiveImage = function() {
+		var window_width = window.innerWidth,
+			desired_resp_obj;
+		for(var i in resp_imgs) {
+			var resp_obj = resp_imgs[i];
+			if(window_width < resp_obj.width) {
+				desired_resp_obj = resp_obj;
+				break;
+			}
+		}
+		$.each($('#hlr-type-container').find('img'), function(index, img) {
+			var $img = $(img);
+			var img_src = $img.data('img-src');
+			$img.attr('src', img_src + desired_resp_obj.tag);
 		});
 	};
-
-	var updateBackgroundOnScrub = function() {
-		var index = Math.floor(($bg_scrubber.position().left/total_width)*NUM_IMAGES);
-		$bg_img_holder.attr('src', img_paths[index]);
-	};
-
-	var animateScrubbing = function() {
-		var left_pos = $bg_scrubber.position().left;
-		if(left_pos + amount_to_move >= total_width)
-			left_pos = 0;
-		else
-			left_pos = left_pos + 1;
-
-		$bg_scrubber.css({
-			left: left_pos
-		});
-		updateBackgroundOnScrub();
-	};
-
-	var hlr_playBGs = function() {
-		if(!animation_id)
-		{
-			animation_id = setInterval(animateScrubbing, animation_delay);
-			$bg_slider.removeClass('hlr-bg-paused');
-			$bg_slider.addClass('hlr-bg-playing');
-		}
-	};
-
-	var hlr_pauseBGs = function() {
-		if(animation_id)
-			clearInterval(animation_id);
-			$bg_timeline.unbind('inview');
-			animation_id = false;
-			$bg_slider.removeClass('hlr-bg-playing');
-			$bg_slider.addClass('hlr-bg-paused');
-	};
-
-	var pressBGButton = function(event) {
-		event.preventDefault();
-		if ($bg_slider.hasClass('hlr-bg-paused')) {
-			hlr_playBGs();
-		} else {
-			hlr_pauseBGs();
-		}
-	};
-
-	$bg_scrubber.draggable({
-		drag: updateBackgroundOnScrub,
-		start: hlr_pauseBGs,
-		scroll:false,
-		grid: [1,0],
-		containment: '.hlr-bg-timeline'
-	});
-
-	$bg_img_holder.bind('inview', function(event, isInView, visiblePartX, visiblePartY) {
-		if(visiblePartY === 'both')
-			hlr_playBGs();
-		else
-			hlr_pauseBGs();
-	});
 
 	// Type Character
 
 	$('.hlr-chars-mod').bind('inview', function(event, isInView, visiblePartX, visiblePartY) {
+
 		if(visiblePartY === 'both') {
 			var $old_seven = $('#hlr-char-oldseven'),
 				$hoefler = $('#hlr-char-hoefler');
 
 			$old_seven.addClass('hlr-char-oldseven');
 			$old_seven.removeClass('start-pos');
+
 			$hoefler.addClass('hlr-char-hoefler');
 			$hoefler.removeClass('start-pos');
 		}
+
 	});
-
-
-	$bg_button.on('click', pressBGButton);
-
-	//Type Slider
-
-	//helper functions
-
-	var _typeResetSize = function(event) {
-		event.preventDefault();
-		$type_mask.css({
-			'width': '50%'
-		});
-	};
-
-	var _typeChangeSize = function(offset) {
-		//console.log(offset);
-		if(offset >= $type_container.width())
-			offset = $type_container.width();
-		else if (event.offsetX <= 0)
-			offset = 0;
-		$type_mask.css({
-			'width': offset
-		});
-	}
-	//bind to input events
-
-	var $type_mask = $('.hlr-type-mask-mod'),
-		$type_container = $('#hlr-type-container');
-
-	$type_container.bind('mouseleave', _typeResetSize);
-
-	$type_container.bind('mousemove', function(event) {
-		//console.log(event);
-		event.preventDefault();
-		_typeChangeSize(event.offsetX);
-	});
-
-	if(Modernizr.touch) {
-		var type_container_node = document.getElementById('hlr-type-container');
-		$type_container.on('touchmove touchstart', function(event) {
-			event.preventDefault();
-			var offset = event.originalEvent.touches[0].pageX - $type_container.offset().left;
-			_typeChangeSize(offset);
-		});
-		$type_container.on('touchend', _typeResetSize);
-	}
-
-	//Window Bindings
-
-	var onResizeFn = function() {
-		total_width = $bg_timeline.width() - $bg_scrubber.width();
-
-		window_width = window.innerWidth;
-		for(var i in resp_imgs) {
-			resp_obj = resp_imgs[i];
-			if(window_width < resp_obj.width) {
-				$.each($type_container.find('img'),function(index, img) {
-					$img = $(img);
-					var img_src = $img.data('img-src');
-					$img.attr('src', img_src + resp_obj.tag);
-				});
-				break;
-			}
-		}
-	};
 
 	$(window).ready(function() {
-		//console.log('ready');
-		onResizeFn();
-	})
-
-	$(window).load(function() {
-		//onResizeFn();
-		setUpImgPaths();
-		preloadImages(img_paths);
-		//console.log('finished preloading');
+		var img_paths = _setUpImgPaths();
+		ts = new TypeScrubber();
+		bs = new BackgroundSlideshow(img_paths);
+		console.log('ready');
+		_findResponsiveImage();
 	});
 
-	$(window).resize(onResizeFn);
+	$(window).load(function() {
+		bs.cacheImages();
+		console.log('done loading');
+	});
+
+	$(window).resize(_findResponsiveImage);
 
 
 })(jQuery);
